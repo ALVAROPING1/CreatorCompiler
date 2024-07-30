240098df1f7c7688ccfc22712eb54c0058f2a856
ALVAROPING1 <alvaroping1@gmail.com>
2024-07-30 18:48:46 +0200

    feat(architecture): add architecture JSON parsing, documentation, and schema generation

---
+++ src/architecture.rs
@@ -0,0 +1,414 @@
+use schemars::{schema_for, JsonSchema};
+use serde::Deserialize;
+
+type Integer = i64;
+
+#[derive(Deserialize, JsonSchema, Debug)]
+#[serde(untagged)]
+enum Number {
+    Int(Integer),
+    Float(f64),
+}
+
+/// Architecture description
+#[derive(Deserialize, JsonSchema, Debug)]
+pub struct Architecture<'a> {
+    /// Metadata about the architecture
+    /// Order of elements is assumed to be name, bits, description, data format,
+    /// memory alignment, main function, passing convention, and sensitive register
+    /// name
+    #[serde(borrow)]
+    arch_conf: [Pair<'a, MetadataKeys>; 8],
+    /// Components (register banks) of the architecture
+    components: [Component<'a>; 3],
+    /// Instructions allowed
+    instructions: Vec<Instruction<'a>>,
+    /// Pseudoinstructions allowed
+    pseudoinstructions: Vec<Pseudoinstruction<'a>>,
+    /// Directives allowed
+    directives: Vec<Directive<'a>>,
+    /// Memory layout of the architecture
+    /// Order of elements is assumed to be text start/end, data start/end, and stack start/end
+    memory_layout: [Pair<'a, MemoryLayoutKeys>; 6],
+}
+
+#[derive(Deserialize, JsonSchema, Debug)]
+struct Pair<'a, Keys> {
+    name: Keys,
+    value: &'a str,
+}
+
+/// Architecture metadata attribute types
+#[derive(Deserialize, JsonSchema, Debug)]
+enum MetadataKeys {
+    /// Name of the architecture
+    Name,
+    /// Word size
+    Bits,
+    /// Description of the architecture
+    Description,
+    /// Storage format of the architecture (big/little endian)
+    #[serde(rename = "Data Format")]
+    DataFormat,
+    /// Whether to enable memory alignment
+    #[serde(rename = "Memory Alignment")]
+    MemoryAlignment,
+    /// Name of the `main` function of the program
+    #[serde(rename = "Main Function")]
+    MainFunction,
+    /// Whether to enable function parameter passing convention checks
+    #[serde(rename = "Passing Convention")]
+    PassingConvention,
+    /// TODO: what does this represent? is this used currently?
+    #[serde(rename = "Sensitive Register Name")]
+    SensitiveRegisterName,
+}
+
+/// Register bank
+#[derive(Deserialize, JsonSchema, Debug)]
+struct Component<'a> {
+    /// Name of the register bank
+    name: &'a str,
+    /// Type of the registers
+    r#type: ComponentType,
+    /// Whether the registers have double the word size
+    double_precision: bool,
+    /// If the registers have double the word size, how this size is achieved
+    double_precision_type: Option<PrecisionType>,
+    /// Registers in this bank
+    elements: Vec<Register<'a>>,
+}
+
+/// Types of register banks allowed
+#[derive(Deserialize, JsonSchema, Debug)]
+enum ComponentType {
+    /// Control registers
+    #[serde(rename = "ctrl_registers")]
+    Ctrl,
+    /// Integer registers
+    #[serde(rename = "int_registers")]
+    Int,
+    /// Floating point registers
+    #[serde(rename = "fp_registers")]
+    Float,
+}
+
+/// Type of registers bigger than a single word
+#[derive(Deserialize, JsonSchema, Debug)]
+#[serde(rename_all = "snake_case")]
+enum PrecisionType {
+    /// Register has a bigger size
+    Extended,
+    /// Register is made up of 2 word size registers
+    Linked,
+}
+
+/// Register specification
+#[derive(Deserialize, JsonSchema, Debug)]
+struct Register<'a> {
+    /// List of aliases
+    name: Vec<&'a str>,
+    /// Size
+    nbits: &'a str,
+    /// Current value of the register
+    value: Number,
+    /// Default value of the register
+    default_value: Number,
+    /// Properties of this register
+    properties: Vec<RegisterProperty>,
+    /// Smaller registers that make up this register when the double precision mode is `Linked`
+    simple_reg: Option<[&'a str; 2]>,
+}
+
+/// Properties of a register
+#[derive(Deserialize, JsonSchema, Debug)]
+#[serde(rename_all = "snake_case")]
+enum RegisterProperty {
+    /// Register can be read
+    Read,
+    /// Register can be written
+    Write,
+    /// Register should be preserved across routine calls
+    Saved,
+    /// Register contains the program counter
+    ProgramCounter,
+    /// Register to save PC in during interruptions
+    ExceptionProgramCounter,
+    /// Interruption ID register
+    EventCause,
+    /// TODO: is this ever used?
+    StatusRegister,
+    /// Writes to this register are ignored. Ignored if `Write` is set
+    IgnoreWrite,
+    /// Register contains the stack pointer
+    StackPointer,
+    /// Register contains the global pointer. Only used in the UI
+    GlobalPointer,
+    /// Register contains the stack frame pointer. Only used in the UI
+    FramePointer,
+}
+
+/// Instruction specification
+#[derive(Deserialize, JsonSchema, Debug)]
+struct Instruction<'a> {
+    /// Name of the instruction
+    name: &'a str,
+    /// Type of the instruction
+    r#type: InstructionType,
+    /// Order of the fields/literal characters in the instruction text. `[fF]\d+` is interpreted as
+    /// the field with index i of the instruction, although the number is ignored and `signature_raw`
+    /// is used instead. Other characters are interpreted literally
+    /// Ex: `F0 F3 F1 (F2)`
+    signature_definition: &'a str,
+    /// Comma-separated list of the type of each field in the instruction, in the order in which
+    /// they appear in the instruction. Valid values are those in `InstructionFieldType`, except
+    /// `Co` and `Cop`. Instruction opcode is specified literally, other characters are interpreted
+    /// literally so that `signature_definition` can capture the value corresponding to each field
+    /// when used as a regex
+    signature: &'a str,
+    /// Same as `signature`, but with a space-separated list of field names
+    #[serde(rename = "signatureRaw")]
+    signature_raw: &'a str,
+    /// Binary op code
+    co: &'a str,
+    /// Binary extended op code
+    cop: &'a str,
+    /// Size of the instruction
+    nwords: Integer,
+    /// Execution time of the instruction
+    clk_cycles: Option<Integer>,
+    /// Parameters of the instruction
+    fields: Vec<InstructionField<'a, BitPosition>>,
+    /// Code to execute for the instruction
+    // Can't be a reference because there might be escape sequences, which require
+    // modifying the data on deserialization
+    definition: String,
+    /// Determines whether the field `i` is separated in the resulting binary instruction
+    separated: Option<Vec<bool>>,
+    /// Help information of the instruction
+    help: &'a str,
+    /// Properties of the instruction
+    properties: Option<Vec<InstructionProperties>>,
+}
+
+/// Allowed instruction types
+#[derive(Deserialize, JsonSchema, Debug)]
+enum InstructionType {
+    #[serde(rename = "Arithmetic integer")]
+    ArithmeticInteger,
+    #[serde(rename = "Arithmetic floating point")]
+    ArithmeticFloat,
+    Logic,
+    Comparison,
+    Control,
+    #[serde(rename = "I/O")]
+    IO,
+    #[serde(rename = "Conditional bifurcation")]
+    ConditionalBifurcation,
+    #[serde(rename = "Unconditional bifurcation")]
+    UnconditionalBifurcation,
+    Syscall,
+    #[serde(rename = "Transfer between registers")]
+    TransferRegister,
+    #[serde(rename = "Memory access")]
+    MemoryAccess,
+    Other,
+}
+
+/// Allowed instruction properties
+#[derive(Deserialize, JsonSchema, Debug)]
+enum InstructionProperties {
+    #[serde(rename = "exit_subrutine")]
+    ExitSubrutine,
+    #[serde(rename = "enter_subrutine")]
+    EnterSubrutine,
+}
+
+/// Instruction field specification
+#[derive(Deserialize, JsonSchema, Debug)]
+struct InstructionField<'a, BitPos> {
+    /// Name of the field
+    name: &'a str,
+    /// Type of the field
+    r#type: InstructionFieldType,
+    /// Starting position of the field. Ignored for pseudoinstructions
+    startbit: BitPos,
+    /// End position of the field. Ignored for pseudoinstructions
+    stopbit: BitPos,
+    /// Fixed value of this field in the binary instruction (specified as a binary string). Only
+    /// used for `Cop` fields
+    #[serde(rename = "valueField")]
+    value_field: Option<&'a str>,
+}
+
+/// Position of the start/end bit of a field in a binary instruction
+#[derive(Deserialize, JsonSchema, Debug)]
+#[serde(untagged)]
+enum BitPosition {
+    // Field uses a single, contiguous bit range
+    Single(Integer),
+    // Field uses multiple, discontiguous bit ranges
+    Multiple(Vec<Integer>),
+}
+
+/// Allowed instruction field types
+#[derive(Deserialize, JsonSchema, Debug)]
+#[serde(rename_all = "snake_case")]
+enum InstructionFieldType {
+    /// Opcode of the instruction
+    Co,
+    /// Extended operation code
+    Cop,
+    /// Inmediatte signed integer
+    #[serde(rename = "inm-signed")]
+    InmSigned,
+    /// Inmediatte unsigned integer
+    #[serde(rename = "inm-unsigned")]
+    InmUnsigned,
+    /// Offset from the instruction address in bytes
+    #[serde(rename = "offset_bytes")]
+    OffsetBytes,
+    /// Offset from the instruction address in words
+    #[serde(rename = "offset_words")]
+    OffsetWords,
+    /// Control register
+    #[serde(rename = "Ctrl-Reg")]
+    CtrlReg,
+    /// Integer register
+    #[serde(rename = "INT-Reg")]
+    IntReg,
+    /// Double precision floating point register
+    #[serde(rename = "DFP-Reg")]
+    DoubleFPReg,
+    /// Single precision floating point register
+    #[serde(rename = "SFP-Reg")]
+    SingleFPReg,
+    /// Inmediatte address, equivalent to `InmUnsigned`
+    Address,
+}
+
+/// Pseudoinstruction specification
+#[derive(Deserialize, JsonSchema, Debug)]
+struct Pseudoinstruction<'a> {
+    /// Name of the pseudoinstruction
+    name: &'a str,
+    /// Order of the fields/literal characters in the instruction text. `[fF]\d+` is interpreted as
+    /// the field with index i of the instruction, although the number is ignored and `signature_raw`
+    /// is used instead. Other characters are interpreted literally
+    /// Ex: `F0 F3 F1 (F2)`
+    signature_definition: &'a str,
+    /// Comma-separated list of the type of each field in the instruction, in the order in which
+    /// they appear in the instruction. Valid values are those in `InstructionFieldType`, except
+    /// `Co` and `Cop`. Instruction opcode is specified literally, other characters are interpreted
+    /// literally so that `signature_definition` can capture the value corresponding to each field
+    /// when used as a regex
+    signature: &'a str,
+    /// Same as `signature`, but with a space-separated list of field names
+    #[serde(rename = "signatureRaw")]
+    signature_raw: &'a str,
+    /// Size of the pseudoinstruction. Ignored since some pseudoinstructions can have different sizes
+    /// depending on the instructions they are replaced with
+    nwords: Integer,
+    /// Parameters of the pseudoinstruction
+    fields: Vec<InstructionField<'a, Option<BitPosition>>>,
+    /// Code to execute for the instruction
+    // Can't be a reference because there might be escape sequences, which require
+    // modifying the data on deserialization
+    definition: String,
+    /// Help information of the instruction
+    help: &'a str,
+    /// Properties of the instruction
+    properties: Option<Vec<InstructionProperties>>,
+}
+
+/// Directive specification
+#[derive(Deserialize, JsonSchema, Debug)]
+struct Directive<'a> {
+    /// Name of the directive
+    name: &'a str,
+    /// Action of the directive
+    action: DirectiveAction,
+    /// Size in bytes of values associated with this directive
+    size: Option<&'a str>,
+}
+
+/// Allowed acytions for directives
+#[derive(Deserialize, JsonSchema, Debug)]
+#[serde(rename_all = "snake_case")]
+enum DirectiveAction {
+    // Switch to the data segment
+    DataSegment,
+    // Switch to the code segment
+    CodeSegment,
+    // Store symbols in an external symbols table
+    GlobalSymbol,
+    // Store n * directive.size null bytes in the data segment. Also sets current alignment to 1
+    Space,
+    // Set current alignment to 2^n bytes
+    Align,
+    // Set current alignment to n bytes
+    Balign,
+    // Store ascii string with a terminating null byte (`\0`) in the data segment. Also sets
+    // current alignment to 1
+    AsciiNullEnd,
+    // Store ascii string without a terminating null byte (`\0`) in the data segment. Also sets
+    // current alignment to 1
+    AsciiNotNullEnd,
+    // Store byte values in the data segment. Also sets current alignment to 1
+    Byte,
+    // Store half word values in the data segment. Also sets current alignment to 1
+    HalfWord,
+    // Store word values in the data segment. Also sets current alignment to 1
+    Word,
+    // Store double word values in the data segment. Also sets current alignment to 1
+    DoubleWord,
+    // Store float values in the data segment. Also sets current alignment to 1
+    Float,
+    // Store double floating point values in the data segment. Also sets current alignment to 1
+    Double,
+}
+
+/// Memory layout attribute keys
+#[derive(Deserialize, JsonSchema, Debug)]
+enum MemoryLayoutKeys {
+    #[serde(rename = "text start")]
+    TextStart,
+    #[serde(rename = "text end")]
+    TextEnd,
+    #[serde(rename = "data start")]
+    DataStart,
+    #[serde(rename = "data end")]
+    DataEnd,
+    #[serde(rename = "stack start")]
+    StackStart,
+    #[serde(rename = "stack end")]
+    StackEnd,
+}
+
+impl<'a> Architecture<'a> {
+    /// Generate a `JSON` schema
+    ///
+    /// # Panics
+    ///
+    /// Panics if the data can't be serialized. This should never happen
+    #[must_use]
+    pub fn schema() -> String {
+        let schema = schema_for!(Architecture);
+        serde_json::to_string_pretty(&schema)
+            .expect("Input is known and fixed, so it shouldn't error out")
+    }
+
+    /// Load architecture data from `JSON`
+    ///
+    /// # Parameters
+    ///
+    /// * `src`: `JSON` data to deserialize
+    ///
+    /// # Errors
+    ///
+    /// Errors if the input `JSON` data is invalid, either because it's ill-formatted or because it
+    /// doesn't conform to the specification
+    pub fn from_json(src: &str) -> serde_json::Result<Architecture> {
+        serde_json::from_str(src)
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use creator_parser::architecture::Architecture;
+
+fn main() {
+    let mut args = std::env::args();
+    args.next();
+    let operation = args
+        .next()
+        .expect("Expected operation code")
+        .parse::<i32>()
+        .expect("Expected operation code to be a number");
+    if operation == 0 {
+        println!("{}", Architecture::schema());
+        return;
+    }
+    let filename = args.next().expect("Expected file argument");
+    let src = std::fs::read_to_string(&filename).expect("We should be able to read the file");
+    if operation == 1 {
+        let arch = Architecture::from_json(&src).expect("The input JSON should be correct");
+        println!("{arch:#?}");
+    }
+}
