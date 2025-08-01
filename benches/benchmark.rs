9d8b4b1d9def7e1b97ca6deb937a73c34d28029b
ALVAROPING1 <alvaroping1@gmail.com>
2025-08-01 23:58:17 +0200

    test: add parse/compile benchmarks

---
+++ benches/benchmark.rs
@@ -0,0 +1,52 @@
+#![allow(clippy::unwrap_used)]
+#![allow(clippy::missing_panics_doc)]
+
+use criterion::{criterion_group, criterion_main, Criterion};
+use std::collections::HashMap;
+use std::hint::black_box;
+use std::time::Duration;
+
+use creator_compiler::parser::ASTNode;
+use creator_compiler::prelude::*;
+
+static ARCH_JSON: &str = include_str!("arch.json");
+static CODE: &str = include_str!("sample.s");
+static NAME: &str = "sample.s";
+
+fn parse(arch: &Architecture) -> Vec<ASTNode> {
+    parser::parse(black_box(arch.arch_conf.comment_prefix), black_box(CODE))
+        .map_err(|e| eprintln!("{}", e.render(NAME, CODE, true)))
+        .unwrap()
+}
+
+pub fn benchmark_parse(c: &mut Criterion) {
+    let arch = Architecture::from_json(ARCH_JSON).unwrap();
+    c.bench_function("parse-only", |b| {
+        b.iter(|| black_box(parse(&arch)));
+    });
+}
+
+pub fn benchmark_compile(c: &mut Criterion) {
+    let arch = Architecture::from_json(ARCH_JSON).unwrap();
+    c.bench_function("compile", |b| {
+        b.iter(|| {
+            let ast = black_box(parse(&arch));
+            black_box(compiler::compile(
+                black_box(&arch),
+                ast,
+                &black_box(0u8.into()),
+                black_box(HashMap::new()),
+                black_box(false),
+            ))
+            .map_err(|e| eprintln!("{}", e.render(NAME, CODE, true)))
+            .unwrap();
+        });
+    });
+}
+
+criterion_group! {
+    name = benches;
+    config = Criterion::default().warm_up_time(Duration::from_secs(5)).measurement_time(Duration::from_secs(15));
+    targets = benchmark_parse, benchmark_compile
+}
+criterion_main!(benches);
